804189a685a517e656e83e8c10df16854901f600
Nicolai Negru <negru.n@gmail.com>
2020-09-06 08:56:23 +0100

    Add upload_detail.html

--- uploads/urls.py
+++ uploads/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import upload_list, new_upload
+from .views import upload_list, new_upload, upload_detail
 
 urlpatterns = [
     path('', upload_list, name="home"),
     path('uploads/', upload_list, name="uploads"),
     path('uploads/new', new_upload, name="new_upload"),
+    path('uploads/<slug:id>', upload_detail, name="upload_detail"),
 ]
